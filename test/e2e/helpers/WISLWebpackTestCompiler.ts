63b0394ff43638098996b4ea8c5731a71137ec6e
Calvin Liang <me@calvin.sh>
2020-12-16 15:56:01 -0800

    test: add back test for query-loader

--- test/e2e/helpers/WISLWebpackTestCompiler.ts
+++ test/e2e/helpers/WISLWebpackTestCompiler.ts
@@ -9,29 +9,60 @@ import {
 interface WISLCompileOptions extends Omit<CompileOptions, "entryFilePath"> {
   entryFileName?: string;
   loaderOptions?: any;
+  useQueryLoader?: boolean;
 }
 
 export default class WISLWebpackTestCompiler extends WebpackTestCompiler {
   compile(options: WISLCompileOptions = {}): Promise<WebpackTestBundle> {
-    const { loaderOptions = {}, entryFileName = "index.js" } = options;
+    const {
+      loaderOptions = {},
+      entryFileName = "index.js",
+      useQueryLoader = false,
+    } = options;
     const fixturesDir = path.resolve(__dirname, "../fixtures");
 
     this.webpackConfig = {
       context: fixturesDir,
       outputPath: path.resolve(__dirname, "../outputs"),
       rules: [
-        {
-          test: /\.(png|jpg|svg)/i,
-          use: [
-            {
-              loader: path.resolve(__dirname, "../../../test-dist/cjs.js"),
-              options: loaderOptions,
+        useQueryLoader
+          ? {
+              test: /\.(png|jpg|svg)/i,
+              use: [
+                {
+                  loader: "webpack-query-loader",
+                  options: {
+                    resourceQuery: "!no-srcset",
+                    use: {
+                      loader: path.resolve(
+                        __dirname,
+                        "../../../test-dist/cjs.js"
+                      ),
+                      options: loaderOptions,
+                    },
+                  },
+                },
+                {
+                  loader: "webpack-query-loader",
+                  options: {
+                    resourceQuery: "!no-srcset",
+                    use: "webpack-image-resize-loader",
+                  },
+                },
+              ],
+            }
+          : {
+              test: /\.(png|jpg|svg)/i,
+              use: [
+                {
+                  loader: path.resolve(__dirname, "../../../test-dist/cjs.js"),
+                  options: loaderOptions,
+                },
+                {
+                  loader: "webpack-image-resize-loader",
+                },
+              ],
             },
-            {
-              loader: "webpack-image-resize-loader",
-            },
-          ],
-        },
       ],
     };
 
---
+++ test/e2e/with-query-loader.test.ts
@@ -0,0 +1,16 @@
+import WISLWebpackTestCompiler from "./helpers/WISLWebpackTestCompiler";
+
+test.each([4, 5] as const)(
+  "use with webpack-query-loader",
+  async (webpackVersion) => {
+    const compiler = new WISLWebpackTestCompiler({ webpackVersion });
+    const bundle = await compiler.compile({
+      loaderOptions: {
+        sizes: ["1x", "300w", null, "2x"],
+      },
+      useQueryLoader: true,
+    });
+
+    expect(bundle.execute("main.js")).toMatchSnapshot("result");
+  }
+);
