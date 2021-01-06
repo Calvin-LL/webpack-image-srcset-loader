50ed09d629b7bee5af6a31a2a7134408aefaddf5
Calvin Liang <me@calvin.sh>
2021-01-06 10:21:19 -0800

    chore: change pre-push test to ci mode

--- husky.config.js
+++ husky.config.js
@@ -2,6 +2,6 @@ module.exports = {
   hooks: {
     "pre-commit": "lint-staged",
     "commit-msg": "commitlint -E HUSKY_GIT_PARAMS",
-    "pre-push": "npm test",
+    "pre-push": "npm test -- --ci",
   },
 };
