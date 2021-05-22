7d9211732c43cc919122c76d0aaf916e89d3b944
Marvin Rösch <marvinroesch99@gmail.com>
2021-05-22 22:57:45 +0200

    fix: fully escape string for usage in require call

--- test/unit/getRequireStringWithModifiedResizeLoaderOptions.test.ts
+++ test/unit/getRequireStringWithModifiedResizeLoaderOptions.test.ts
@@ -54,7 +54,7 @@ it("should handle resizeLoaderPath options being string", async () => {
   );
 
   expect(result).toMatchInlineSnapshot(
-    `"<rootDir>/node_modules/file-loader/dist/cjs.js??ruleSet[1].rules[1].use[0]!<rootDir>/node_modules/webpack-image-resize-loader/dist/cjs.js?{"test":3,"scaleUp":true}!/home/username/project-name/src/assets/Macaca_nigra_self-portrait_large.jpg"`
+    `"<rootDir>/node_modules/file-loader/dist/cjs.js??ruleSet[1].rules[1].use[0]!<rootDir>/node_modules/webpack-image-resize-loader/dist/cjs.js?{\\"test\\":3,\\"scaleUp\\":true}!/home/username/project-name/src/assets/Macaca_nigra_self-portrait_large.jpg"`
   );
 });
 
@@ -106,7 +106,7 @@ it("should handle resizeLoaderPath options being object", async () => {
   );
 
   expect(result).toMatchInlineSnapshot(
-    `"<rootDir>/node_modules/file-loader/dist/cjs.js??ruleSet[1].rules[1].use[0]!<rootDir>/node_modules/webpack-image-resize-loader/dist/cjs.js?{"test":3,"scaleUp":true}!/home/username/project-name/src/assets/Macaca_nigra_self-portrait_large.jpg"`
+    `"<rootDir>/node_modules/file-loader/dist/cjs.js??ruleSet[1].rules[1].use[0]!<rootDir>/node_modules/webpack-image-resize-loader/dist/cjs.js?{\\"test\\":3,\\"scaleUp\\":true}!/home/username/project-name/src/assets/Macaca_nigra_self-portrait_large.jpg"`
   );
 });
 
@@ -158,7 +158,7 @@ it("should handle resizeLoaderPath options being undefined", async () => {
   );
 
   expect(result).toMatchInlineSnapshot(
-    `"<rootDir>/node_modules/file-loader/dist/cjs.js??ruleSet[1].rules[1].use[0]!<rootDir>/node_modules/webpack-image-resize-loader/dist/cjs.js?{"scaleUp":true}!/home/username/project-name/src/assets/Macaca_nigra_self-portrait_large.jpg"`
+    `"<rootDir>/node_modules/file-loader/dist/cjs.js??ruleSet[1].rules[1].use[0]!<rootDir>/node_modules/webpack-image-resize-loader/dist/cjs.js?{\\"scaleUp\\":true}!/home/username/project-name/src/assets/Macaca_nigra_self-portrait_large.jpg"`
   );
 });
 
@@ -264,6 +264,58 @@ it("should handle resizeLoaderOptionsGenerator", async () => {
   );
 
   expect(result).toMatchInlineSnapshot(
-    `"<rootDir>/node_modules/file-loader/dist/cjs.js??ruleSet[1].rules[1].use[0]!<rootDir>/node_modules/webpack-image-resize-loader/dist/cjs.js?{"test":"true"}!/home/username/project-name/src/assets/Macaca_nigra_self-portrait_large.jpg"`
+    `"<rootDir>/node_modules/file-loader/dist/cjs.js??ruleSet[1].rules[1].use[0]!<rootDir>/node_modules/webpack-image-resize-loader/dist/cjs.js?{\\"test\\":\\"true\\"}!/home/username/project-name/src/assets/Macaca_nigra_self-portrait_large.jpg"`
+  );
+});
+
+it("should fully escape JS string for use in require", async () => {
+  const fileLoaderPath = require.resolve("file-loader");
+  const resizeLoaderName = "webpack-image-resize-loader";
+  const resizeLoaderPath = require.resolve(resizeLoaderName);
+  const remainingRequest = `${fileLoaderPath}??ruleSet[1].rules[1].use[0]!${resizeLoaderPath}??ruleSet[1].rules[1].use[1]!/home/username/project-name/src/assets/Macaca_nigra_'self-portrait'_large.jpg`;
+  const loaders: {
+    path: string;
+    request: string;
+    options?: Record<string, any> | string;
+  }[] = [
+    {
+      path: "webpack-image-srcset-loader",
+      request: 'webpack-image-srcset-loader?{"test":5}',
+      options: undefined,
+    },
+    {
+      path: fileLoaderPath,
+      request: `${fileLoaderPath}??ruleSet[1].rules[1].use[0]`,
+      options: { test: 5 },
+    },
+    {
+      path: resizeLoaderPath,
+      request: `${resizeLoaderPath}??ruleSet[1].rules[1].use[1]`,
+      options: '{"test":3}',
+    },
+  ];
+  const loaderIndex = 0;
+  const options = {};
+  const resizeLoaderOptionsGenerator = defaultResizeLoaderOptionsGenerator;
+  const context = { loaders, loaderIndex } as loader.LoaderContext;
+
+  jest.doMock("../../src/helpers/resolveLoader", () => ({
+    __esModule: true,
+    default: async () => resizeLoaderPath,
+  }));
+
+  const getRequireString = require("../../src/helpers/getRequireStringWithModifiedResizeLoaderOptions")
+    .default;
+
+  const result = await getRequireString(
+    context,
+    remainingRequest,
+    options,
+    resizeLoaderName,
+    resizeLoaderOptionsGenerator
+  );
+
+  expect(result).toMatchInlineSnapshot(
+    `"<rootDir>/node_modules/file-loader/dist/cjs.js??ruleSet[1].rules[1].use[0]!<rootDir>/node_modules/webpack-image-resize-loader/dist/cjs.js?{\\"test\\":3,\\"scaleUp\\":true}!/home/username/project-name/src/assets/Macaca_nigra_\\'self-portrait\\'_large.jpg"`
   );
 });
